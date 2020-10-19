cae5d44ffc7033be3cb65520cfe5ae3056b0f979
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2020-10-18 18:49:06 -0700

    Add OwO API to HewoAPI (which just delegates back to OwO) so that consumers only need to worry about one API :)
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- src/main/java/ai/arcblroth/fabric/hewo/HewoAPIImpl.java
+++ src/main/java/ai/arcblroth/fabric/hewo/HewoAPIImpl.java
@@ -1,8 +1,11 @@
 package ai.arcblroth.fabric.hewo;
 
 import ai.arcblroth.fabric.hewo.api.HewoAPI;
+import maow.owo.OwO;
+import maow.owo.util.json.Substitution;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
@@ -12,7 +15,8 @@ public class HewoAPIImpl implements HewoAPI {
     public static final HewoAPIImpl HEWO = new HewoAPIImpl();
     private ArrayList<Supplier<Double>> chances = new ArrayList<>();
 
-    private HewoAPIImpl() {}
+    private HewoAPIImpl() {
+    }
 
     @Override
     public void addOwoProbabilityModifier(Supplier<Double> probabilityModifier) {
@@ -21,6 +25,34 @@ public class HewoAPIImpl implements HewoAPI {
 
     public boolean shouldOwo() {
         return ThreadLocalRandom.current().nextDouble() < chances.stream().map(Supplier::get).max(Double::compareTo).orElse(-1D);
+    }
+
+    public List<Substitution> getSubstitutions() {
+        return OwO.INSTANCE.getSubstitutions();
+    }
+
+    public List<String> getPrefixes() {
+        return OwO.INSTANCE.getPrefixes();
+    }
+
+    public List<String> getSuffixes() {
+        return OwO.INSTANCE.getSuffixes();
+    }
+
+    public void addSubstitution(Substitution substitution) {
+        OwO.INSTANCE.addSubstitution(substitution);
+    }
+
+    public void addSubstitution(String original, String substitute, boolean byItself) {
+        OwO.INSTANCE.addSubstitution(original, substitute, byItself);
+    }
+
+    public void addPrefix(String prefix) {
+        OwO.INSTANCE.addPrefix(prefix);
+    }
+
+    public void addSuffix(String suffix) {
+        OwO.INSTANCE.addPrefix(suffix);
     }
 
 }
--- src/main/java/ai/arcblroth/fabric/hewo/api/HewoAPI.java
+++ src/main/java/ai/arcblroth/fabric/hewo/api/HewoAPI.java
@@ -1,7 +1,9 @@
 package ai.arcblroth.fabric.hewo.api;
 
 import ai.arcblroth.fabric.hewo.HewoAPIImpl;
+import maow.owo.util.json.Substitution;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public interface HewoAPI {
@@ -18,8 +20,45 @@ public interface HewoAPI {
      * one modifier returns 0.4 and another returns 0.6,
      * than the overall probability for owofication
      * will be 0.6.
-     * @param probabilityModifier
      */
     void addOwoProbabilityModifier(Supplier<Double> probabilityModifier);
+
+    /**
+     * Gets all {@link maow.owo.util.json.Substitution}s
+     * registered on the owofier.
+     */
+    public List<Substitution> getSubstitutions();
+
+    /**
+     * Gets all prefixes registered on the owofier.
+     */
+    public List<String> getPrefixes();
+
+    /**
+     * Gets all suffixes registered on the owofier.
+     */
+    public List<String> getSuffixes();
+
+    /**
+     * Adds a {@link maow.owo.util.json.Substitution}
+     * to the owofier.
+     */
+    public void addSubstitution(Substitution substitution);
+
+    /**
+     * Builds a {@link maow.owo.util.json.Substitution}
+     * and adds it to the owoifer.
+     */
+    public void addSubstitution(String original, String substitute, boolean byItself);
+
+    /**
+     * Adds a prefix to the owofier.
+     */
+    public void addPrefix(String prefix);
+
+    /**
+     * Adds a sufix to the owofier.
+     */
+    public void addSuffix(String suffix);
 
 }
