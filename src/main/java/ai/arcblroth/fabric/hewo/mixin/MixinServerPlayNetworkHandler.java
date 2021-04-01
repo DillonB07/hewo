f4334bec0a87999158cf75cb2028c01d9bc1141b
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2021-04-01 09:52:09 -0700

    prepare for april fools update
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- src/main/java/ai/arcblroth/fabric/hewo/mixin/MixinServerPlayNetworkHandler.java
+++ src/main/java/ai/arcblroth/fabric/hewo/mixin/MixinServerPlayNetworkHandler.java
@@ -12,14 +12,16 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(ServerPlayNetworkHandler.class)
 public class MixinServerPlayNetworkHandler {
 
-    @ModifyArg(method = "method_31286", at = @At(value = "INVOKE", target = "net/minecraft/server/PlayerManager.broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"), index = 0)
+    @ModifyArg(method = "method_31286", at = @At(value = "INVOKE", target = "net/minecraft/server/PlayerManager.broadcast(Lnet/minecraft/text/Text;Ljava/util/function/Function;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V"), index = 0)
     private Text owoifyChatMessage(Text text) {
         if(HewoAPIImpl.HEWO.shouldOwo()) {
             if (text instanceof TranslatableText) {
                 TranslatableText translatableText = (TranslatableText) text;
-                Object[] args = translatableText.getArgs();
-                if (args.length >= 2) {
-                    args[1] = OwO.INSTANCE.owo(args[1].toString(), 256);
+                if(translatableText.getKey().equals("chat.type.text")) {
+                    Object[] args = translatableText.getArgs();
+                    if (args.length >= 2) {
+                        args[1] = OwO.INSTANCE.owo(args[1].toString(), 256);
+                    }
                 }
             }
         }
